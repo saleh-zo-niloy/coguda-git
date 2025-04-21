f8701a626b6a2fe21110ba3fa1f94a9edc60f491
AzureAD\MDSALEHZOHURBHUIYAN <saleh@erainfotechbd.com>
2025-04-21 12:36:29 +0600

    testing-library-updates

--- projects/demo-ui-erangx/src/lib/components/text-input/text-input.component.ts
+++ projects/demo-ui-erangx/src/lib/components/text-input/text-input.component.ts
@@ -8,7 +8,7 @@ import { Component, Input } from '@angular/core';
   styleUrl: '../../style.scss',
 })
 export class TextInputComponent {
-  @Input() label = '';
+  @Input() labe = '';
 
   inputValue = '';
 
